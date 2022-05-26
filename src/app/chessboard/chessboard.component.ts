486e433db8e058eb74274842bd74e3e77961640f
Ostojλ <ostojasredojevic@protonmail.com>
2022-05-26 04:01:12 +0200

    Fixed checker board.

--- src/app/chessboard/chessboard.component.ts
+++ src/app/chessboard/chessboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from './../shared.service';
 
 import * as ChessJS from "chess.js";
 
@@ -14,30 +15,34 @@ export class ChessboardComponent implements OnInit {
 
   first!: number;
 
-  constructor() {
+  constructor(private sharedService:SharedService) {
     const Chess = typeof ChessJS === "function" ? ChessJS : ChessJS.Chess;
     this.chess = new Chess();
   }
 
   ngOnInit(): void {
+    this.square = Array(64).fill("");
     this.newGame();
   }
 
   newGame() {
     this.chess.reset();
-    this.square = Array(64).fill(null);
+    this.square.fill("");
     this.winner = null;
     this.first = -1;
-    var i = 0;
+    let i = 0;
     this.chess.board().forEach((element: any) => {
       for(var j in element) {
-        if(!element[j]) 
-          this.square.splice(i, 1, "");
-        else
-          this.square.splice(i, 1, element[j].type);
+        if(element[j] == null) 
+          this.square[i] = null;
+        else {
+          console.log(element[j].type);
+          this.square[i] = element[j].type;
+        }
         i++;
       }
     });
+    this.sharedService.sendClickEvent();
   }
 
   get player() {
@@ -52,17 +57,23 @@ export class ChessboardComponent implements OnInit {
         if(this.chess.moves({ square: `${this.numToCord(this.first)}` }).length != 0) {
           this.chess.move(`${this.numToCord(this.first)}${this.numToCord(idx)}`, { sloppy: true });
           var i = 0;
+          this.square.fill("");
           this.chess.board().forEach((element: any) => {
             for(var j in element) {
-              if(!element[j]) 
-                this.square.splice(i, 1, "");
-              else
-                this.square.splice(i, 1, element[j].type);
+              if(element[j] == null) {
+                this.square[i] = "";
+              }
+              else {
+                console.log(element[j].type);
+                this.square[i] = element[j].type;
+              }
               i++;
             }
           });
         }
+        console.log(this.chess.ascii());
         this.first = -1;
+        this.sharedService.sendClickEvent();
       }
     }
     if(this.chess.game_over() == true) {
--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,19 +1,46 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { SharedService } from './../shared.service';
+import { Subscription } from 'rxjs';
+
+import { CheckboxControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-square',
   template: `
-    <button nbButton *ngIf="!value" [ngStyle]="{'background-color': num%2==0 ? '#edd5bd' : '#79534c'}">{{ value }}</button>
-    <button nbButton *ngIf="value == 'p'" [ngStyle]="{'background-color': num%2==0 ? '#edd5bd' : '#79534c'}" >{{ value }}</button>
-    <button nbButton *ngIf="value == 'r'" [ngStyle]="{'background-color': num%2==0 ? '#edd5bd' : '#79534c'}">{{ value }}</button>
-    <button nbButton *ngIf="value == 'n'" [ngStyle]="{'background-color': num%2==0 ? '#edd5bd' : '#79534c'}">{{ value }}</button>
-    <button nbButton *ngIf="value == 'b'" [ngStyle]="{'background-color': num%2==0 ? '#edd5bd' : '#79534c'}">{{ value }}</button>
-    <button nbButton *ngIf="value == 'q'" [ngStyle]="{'background-color': num%2==0 ? '#edd5bd' : '#79534c'}">{{ value }}</button>
-    <button nbButton *ngIf="value == 'k'" [ngStyle]="{'background-color': num%2==0 ? '#edd5bd' : '#79534c'}">{{ value }}</button>
+    <button nbButton *ngIf="!value" [ngStyle]="{'background-color': color}">{{ value }}</button>
+    <button nbButton *ngIf="value == 'p'" [ngStyle]="{'background-color': color}" >{{ value }}</button>
+    <button nbButton *ngIf="value == 'r'" [ngStyle]="{'background-color': color}">{{ value }}</button>
+    <button nbButton *ngIf="value == 'n'" [ngStyle]="{'background-color': color}">{{ value }}</button>
+    <button nbButton *ngIf="value == 'b'" [ngStyle]="{'background-color': color}">{{ value }}</button>
+    <button nbButton *ngIf="value == 'q'" [ngStyle]="{'background-color': color}">{{ value }}</button>
+    <button nbButton *ngIf="value == 'k'" [ngStyle]="{'background-color': color}">{{ value }}</button>
   `,
   styleUrls: ['./square.component.scss']
 })
-export class SquareComponent {
+export class SquareComponent implements OnInit {
+  clickEventsubscription:Subscription;
+  constructor(private sharedService:SharedService) {
+    this.clickEventsubscription = this.sharedService.getClickEvent().subscribe(() => {
+    this.checkColor();
+    })
+  }
   @Input() value!: string;
   @Input() num!: number;
+  @Input() color!: string;
+
+  ngOnInit(): void {
+    this.checkColor();
+  }
+
+  checkColor() {
+    setTimeout(() => {
+      if(Math.floor((this.num/8))%2 == 0) {
+        this.color = this.num%2==0 ? '#edd5bd' : '#79534c';
+      } else {
+        this.color = this.num%2==0 ? '#79534c' : '#edd5bd';
+      }
+    }, 50);
+    console.log("CHECKING COLOR!");
+    console.log(this.num);
+  }
 }
